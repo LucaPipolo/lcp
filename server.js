74a504686f051bee007512211b800a139cc8ea4d
Luca Pipolo <dev@lucapipolo.com>
2017-07-21 21:08:11 +0200

    Fixed Eslint errors on server.js.

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var compression = require('compression');
-var path = require('path');
-var port = process.env.PORT || 3000;
-var app = express();
+var express = require('express')
+var compression = require('compression')
+var path = require('path')
+var port = process.env.PORT || 3000
+var app = express()
 
-app.use(compression());
-app.use(express.static(path.join(__dirname, './dist')));
-app.get('/', function (request, response){
-  response.sendFile(path.join(__dirname, './dist/index.html'));
-});
-app.listen(port);
+app.use(compression())
+app.use(express.static(path.join(__dirname, './dist')))
+app.get('/', function (request, response) {
+  response.sendFile(path.join(__dirname, './dist/index.html'))
+})
+app.listen(port)
